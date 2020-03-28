066f6b772b5962c0c9a829daf1ab470a106802eb
sudnyeshtalekar <sudnyeshtalekar@gmail.com>
2020-03-28 23:37:10 +0530

    JSON parsing for global stats

--- app/src/main/java/com/sud/covid_19/HomeActivity.java
+++ app/src/main/java/com/sud/covid_19/HomeActivity.java
@@ -2,7 +2,22 @@ package com.sud.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +25,83 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://thevirustracker.com/free-api?global=stats");
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+
+           try {
+
+                JSONObject jsonObject = new JSONObject(result);
+
+                String result1 = jsonObject.getString("results");
+
+                Log.i("result", result1);
+
+                JSONArray arr = new JSONArray(result1);
+
+                for (int i = 0; i < arr.length(); i++) {
+
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    Log.i("total_cases", jsonPart.getString("total_cases"));
+                    Log.i("total_recovered", jsonPart.getString("total_recovered"));
+
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
     }
 }
